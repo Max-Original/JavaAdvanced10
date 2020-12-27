07e1b95ba6a612786ac2ad317ddd431a705e9d26
Max Original <max.original666@gmail.com>
2020-12-27 23:47:21 +0200

    add filters to the project, add show/delete product from the basket, add some security.

---
+++ javaAdvanced-10/src/main/java/dto/BasketDto.java
@@ -0,0 +1,12 @@
+package dto;
+
+import java.util.Date;
+
+public class BasketDto {
+	
+	public Integer basketId;
+	public String name;
+	public String description;
+	public Double price;
+	public Date purchaseDate;
+}
